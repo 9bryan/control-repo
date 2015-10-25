4b475ad58ff620ec09e7ca67bf65fdb1f27c685c
Jeremy Adams <jeremy@puppetlabs.com>
2015-10-24 19:25:17 -0700

    Add webhook definitions

--- site/profile/manifests/gitlab.pp
+++ site/profile/manifests/gitlab.pp
@@ -19,6 +19,26 @@ class profile::gitlab {
   class { 'gitlab':
     external_url => hiera( 'gms_server_url', "https://${::fqdn}") ,
     require => File["/etc/gitlab/ssl/${fqdn}.key", "/etc/gitlab/ssl/${fqdn}.key"],
+  } ->
+
+  git_webhook { 'web_post_receive_webhook' :
+    ensure               => present,
+    webhook_url          => 'https://master.inf.puppetlabs.com:8088/payload',
+    token                => hiera('gms_api_token'),
+    merge_request_events => true,
+    project_name         => 'puppet/control-repo',
+    server_url           => 'http://centos6b.syd.puppetlabs.demo',
+    provider             => 'gitlab',
+  } ->
+
+  git_webhook { 'web_post_receive_webhook' :
+    ensure               => present,
+    webhook_url          => 'https://master.inf.puppetlabs.com:8088/module',
+    token                => hiera('gms_api_token'),
+    merge_request_events => true,
+    project_name         => 'puppet/control-repo',
+    server_url           => 'http://centos6b.syd.puppetlabs.demo',
+    provider             => 'gitlab',
   }
 
 }
