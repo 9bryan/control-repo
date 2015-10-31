7ad7a16420a925d753efbcb371b4118b19ebbea5
Jeremy Adams <jeremy@puppetlabs.com>
2015-10-31 13:10:16 -0700

    open puppetdb firewall port on MoM

--- site/profile/manifests/puppetmaster/firewall.pp
+++ site/profile/manifests/puppetmaster/firewall.pp
@@ -9,5 +9,7 @@ class profile::puppetmaster::firewall {
   firewall { '200 allow mco': dport => '61613', }
   firewall { '300 allow console https': dport => '443', }
   firewall { '400 allow webhook call from gms': dport => '8088', }
+  firewall { '500 allow puppetdb https': dport => '8081', }
+  firewall { '600 allow classifer https': dport => '4433', }
 
 }
