6f72c830a1253f1c10cde804db0b31e43ce24c29
Jeremy Adams <jeremy@puppetlabs.com>
2015-10-31 20:52:28 -0700

    compile masters need webhook firewall rule too

--- site/profile/manifests/puppetmaster/firewall.pp
+++ site/profile/manifests/puppetmaster/firewall.pp
@@ -8,11 +8,11 @@ class profile::puppetmaster::firewall {
   }
 
   firewall { '100 allow puppet': dport => '8140', }
+  firewall { '200 allow webhook call from gms': dport => '8088', }
 
   if $::trusted['extensions']['pp_role'] == 'master_of_masters' {
-    firewall { '200 allow mco': dport => '61613', }
-    firewall { '300 allow console https': dport => '443', }
-    firewall { '400 allow webhook call from gms': dport => '8088', }
+    firewall { '300 allow mco': dport => '61613', }
+    firewall { '400 allow console https': dport => '443', }
     firewall { '500 allow puppetdb https': dport => '8081', }
     firewall { '600 allow classifer https': dport => '4433', }
   }
